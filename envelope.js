451fe2110ae2782160a4cf09db5029bd89b611e9
gianne4002 <gianne4002@gmail.com>
2025-02-05 13:16:34 -0500

    update

--- envelope.js
+++ envelope.js
@@ -10,6 +10,10 @@ document.addEventListener("DOMContentLoaded", function () {
     let overlay = document.getElementById("overlay");
     let h1 = document.getElementById("h1");
     let randomImage = document.getElementById("randomImage");
+    let tokensmall = document.getElementById("tokensmall");
+    let h2 = document.getElementById("h2");
+    let envelopeSound = document.getElementById("envelopeSound");
+
 
     const images = [
         "resources/note1.png",
@@ -23,6 +27,14 @@ document.addEventListener("DOMContentLoaded", function () {
         envelope.style.display = "none";
         envelopeOpen.style.display = "block";
         button.style.display = "block";
+        envelopeSound.currentTime = 0; 
+        envelopeSound.playbackRate = 0.85;
+        envelopeSound.volume = 0.3;
+        envelopeSound.play();
+        setTimeout(function () {
+            envelopeSound.pause(); 
+            envelopeSound.currentTime = 0; 
+        }, 700);
     });
 
     button.addEventListener("click", function () {
@@ -42,6 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
         clouds.style.display = "none";
         video.style.display = "block"; 
         video.play(); 
+        clouds.value = "";
     });
 
     video.addEventListener("ended", function () {
@@ -57,10 +70,33 @@ document.addEventListener("DOMContentLoaded", function () {
     capsule.addEventListener("click", function () {
         h1.style.display = "none";
         capsule.style.display = "none";
+
         let randomIndex = Math.floor(Math.random() * images.length);
         randomImage.src = images[randomIndex];
         randomImage.style.display = "block";
-        overlay.style.display = "none";
+        tokensmall.style.display = "block"; 
+        h2.style.display = "block";
     });
-});
 
+    tokensmall.addEventListener("click", function () {
+        randomImage.style.display = "none";
+        token.classList.add("show");
+        token.style.display = "block"; 
+        button2.style.display = "block"; 
+        clouds.style.display = "block"; 
+        tokensmall.style.display = "none"; 
+        overlay.style.display = "none";
+        h2.style.display = "none";
+
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        let envelope = document.getElementById("envelope");
+    
+        function stopAnimation() {
+            if (envelope.classList.contains("bottom-left")) {
+                envelope.style.animation = "none"; 
+            }
+        }
+    });
+    })
